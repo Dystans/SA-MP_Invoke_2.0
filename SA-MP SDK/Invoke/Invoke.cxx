7eae0c45fd35bd552e21f491a4f1b8672572158f
Dystans <dystans.0@gmail.com>
2014-03-24 11:58:05 +0100

    uploading
    Uploaded everything on GitHub.

---
+++ SA-MP SDK/Invoke/Invoke.cxx
@@ -0,0 +1,137 @@
+/*
+    SA-MP 0.3z Invoke v1.0
+    Copyright (c) 2014, Linux_Shines
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+        * Redistributions of source code must retain the above copyright
+          notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of the Linux_Shines nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL LINUX_SHINES BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "Invoke.hxx"
+
+Invoke * g_Invoke;
+#define callNative Call_Native 
+
+int Invoke::Call_Native(const INVOKE::Native * native, ...) {
+	if(amx_list.empty() || amx_map.find(native->name) == amx_map.end()) {
+		return false;
+	}
+
+	unsigned int amx_addr = amx_map[native->name], count = strlen(native->data), variables = 0;
+	cell * params = new cell[count + 1], * physAddr[6];
+	params[0] = count * sizeof(cell);
+	va_list input;
+	va_start(input, native);
+	for(unsigned int i = 0; i < count; ++i) {
+		switch(native->data[i]) {
+			case 'd': {
+				params[i + 1] = va_arg(input, int);
+			}
+			case 'f': {
+				float value = (float) va_arg(input, double);
+				params[i + 1] = amx_ftoc(value);
+			}
+			break;
+			case 'i': {
+				params[i + 1] = va_arg(input, int);
+			}
+			break;
+			case 'p': {
+				va_arg(input, void *);
+				int size = va_arg(input, int);
+				amx_Allot(amx_list.front(), size, &params[++i], &physAddr[variables++]);
+				params[i + 1] = size;
+			}
+			break;
+			case 's': {
+				char * string = va_arg(input, char *);
+				amx_Allot(amx_list.front(), strlen(string) + 1, &params[i + 1], &physAddr[variables++]);
+				amx_SetString(physAddr[variables - 1], string, 0, 0, strlen(string) + 1);
+			}
+			break;
+			case 'v': {
+				va_arg(input, void *);
+				amx_Allot(amx_list.front(), 1, &params[i + 1], &physAddr[variables++]);
+			}
+			break;
+		}
+	}
+	va_end(input);
+	amx_Function_t amx_Function = (amx_Function_t) amx_addr;
+	int value = amx_Function(amx_list.front(), params);
+	if(variables) {
+		variables = 0;
+		va_start(input, native);
+		for(unsigned int i = 0; i < count; ++i) {
+			switch(native->data[i]) {
+				case 'p': {
+					char * text = va_arg(input, char *);
+					int size = va_arg(input, int);
+					amx_GetString(text, physAddr[variables++], 0, size);
+					amx_Release(amx_list.front(), params[++i]);
+				}
+				break;
+				case 's': {
+					amx_Release(amx_list.front(), params[i + 1]);
+				}
+				break;
+				case 'v': {
+					unsigned int * value = va_arg(input, unsigned int *), * returnValue = (unsigned int *) physAddr[variables++];
+					* value = * returnValue;
+					amx_Release(amx_list.front(), params[i + 1]);
+				}
+				break;
+				default: {
+					va_arg(input, void *);
+				}
+				break;
+			}
+		}
+		va_end(input);
+	}
+	delete[] params;
+	return value;
+}
+
+int Invoke::getAddresses() {
+	if(gotAddresses) {
+		return true;
+	}
+	AMX_HEADER * amx_hdr = (AMX_HEADER *) (amx_list.back())->base;
+	std::size_t size = sizeof(INVOKE::names) / sizeof(const char *);
+	for(std::size_t i = 0; i < size; ++i) {
+		amx_FindNative(amx_list.back(), INVOKE::names[i], &amx_idx);
+		if(amx_idx != std::numeric_limits <int>::max()) {
+			unsigned int amx_addr = (unsigned int) ((AMX_FUNCSTUB *) ((char *) amx_hdr + amx_hdr -> natives + amx_hdr -> defsize * amx_idx)) -> address;
+			if(amx_addr) {
+				if(amx_map.find(INVOKE::names[i]) == amx_map.end()) {
+					amx_map.insert(std::make_pair(INVOKE::names[i], amx_addr));
+				}
+			}
+		}
+	}
+	if(amx_map.size() == size) {
+		gotAddresses = true;
+		return true;
+	}
+	return false;
+}
---
+++ SA-MP SDK/SA-MP SDK.cxx
@@ -0,0 +1,59 @@
+#include "Invoke\Invoke.hxx"
+
+typedef void (*logprintf_t) (char* format, ...);
+logprintf_t logprintf;
+void ** ppPluginData;
+extern void * pAMXFunctions;
+
+cell AMX_NATIVE_CALL Testing(AMX* amx, cell* params) {
+	float
+		x = NULL,
+		y = NULL,
+		z = NULL;
+
+	//Get the player's position (and check to see if he is even connected).
+	if(g_Invoke->Call_Native(&INVOKE::GetPlayerPos, params[1], &x, &y, &z)) {
+		char name[24];
+
+		//Get the rest of the player's information (name, interior, and virtualworld) and print it.
+		g_Invoke->Call_Native(&INVOKE::GetPlayerName, params[1], name);
+		int interior = g_Invoke->Call_Native(&INVOKE::GetPlayerInterior, params[1]);
+		int virtualworld = g_Invoke->Call_Native(&INVOKE::GetPlayerVirtualWorld, params[1]);
+
+		logprintf("%s is at X: %.2f, Y: %.2f, Z: %.2f (Virtual world: %d, Interior %d).", name, x, y, z, virtualworld, interior);
+		return 1;
+
+	}
+	return 0;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData) {
+	pAMXFunctions = ppData[PLUGIN_DATA_AMX_EXPORTS];
+	logprintf = (logprintf_t) ppData[PLUGIN_DATA_LOGPRINTF];
+	return true;
+}
+
+PLUGIN_EXPORT void PLUGIN_CALL Unload() {
+}
+
+static cell AMX_NATIVE_CALL Test(AMX * amx, cell * params) {
+	logprintf("Test");
+	return true;
+}
+
+AMX_NATIVE_INFO projectNatives[] = {
+	{"Test", Test},
+};
+
+
+PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports() {
+	return SUPPORTS_VERSION | SUPPORTS_AMX_NATIVES;
+}
+
+PLUGIN_EXPORT int PLUGIN_CALL AmxLoad(AMX *amx) {
+	return amx_Register(amx, projectNatives, -1);
+}
+
+PLUGIN_EXPORT int PLUGIN_CALL AmxUnload(AMX *amx) {
+	return AMX_ERR_NONE;
+}
